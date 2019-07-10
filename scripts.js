a3215680702d744da63315af25814729bbb5ba81
RC Patterson <patteso1849@gmail.com>
2019-07-09 20:48:35 -0500

    all the shit we just did like adding the javascript to the page

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Create a greeter function "fxn." that will use the persons name and display 'Hello, John' in <h1>
+
+/*
+  1. Ask the user for their name.
+  2. Put that in memory.
+  3. Construct, or concatinate, a string with 'hello' and the value stored in memory for their name.
+  4.Select the <h1> on the page.
+  5. Replace the value of the <h1> tag.
+*/
+const name = prompt('What is your name?');
+console.log(name);
