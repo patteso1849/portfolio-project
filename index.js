00fd36d20f8c0a9a1759b45fda959de4340da171
RC Patterson <patteso1849@gmail.com>
2019-07-16 13:52:11 -0500

    Adding package.json, new file index.js and all current changes

---
+++ index.js
@@ -0,0 +1,8 @@
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+document.querySelector('#root').innerHTML
+
+console.log(Navigation, Header, Main, Footer)
