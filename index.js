952903c90187d604a1d6fb0301da181602194126
RC Patterson <patteso1849@gmail.com>
2019-07-29 18:31:44 -0500

    index files, html and js

--- index.js
+++ index.js
@@ -33,33 +33,44 @@ const navItems = document.querySelectorAll('nav > ul > li:not(.dropdown)');
 
 console.log(navItems);
 
-let i = 0;
+
+navItems.forEach(function eventListenAdder(navItem){
+    navItem.addEventListener('click' , function(event){
+        event.preventDefault();
+
+
+        console.log(event.target.textContent);
+    });
+});
+
+// let i = 0;
 // TODO: Refactor this loop structure.
 // i will run from 0 until 3
 
-while(i < navItems.length){
-    // console.log(preventDefault);
+// while(i < navItems.length){
+//     // console.log(preventDefault);
 
-    // use the value of i as an index to access the navItem.
+//     // use the value of i as an index to access the navItem.
 
-    navItems[i].addEventListener('click',
-
-        // add this call back function to each of the navItems.
-        function clickhandler(event){
-            event.preventDefault();
-
-            // console.log('hello');
-            // target will revealwhta generated the event  - that was 'clicked'.
-            const clickedItem = event.target.textContent;
-            const clicked = clickedItem.toLowerCase();
+//     navItems[i].addEventListener('click',
 
 
-            render(states[clicked]);
-        });
-    i += 1;
-}
+//         // add this call back function to each of the navItems.
+//         function clickhandler(event){
+//             event.preventDefault();
 
-// .addEventListener
+//             // console.log('hello');
+//             // target will revealwhta generated the event  - that was 'clicked'.
+//             const clickedItem = event.target.textContent;
+//             const clicked = clickedItem.toLowerCase();
 
-// we want to re-render out page based on what the user clicks in our navigation menu.
+
+//             render(states[clicked]);
+//         });
+//     i += 1;
+// }
+
+// // .addEventListener
+
+// // we want to re-render out page based on what the user clicks in our navigation menu.
 // we need to re-render the page with the need view.
